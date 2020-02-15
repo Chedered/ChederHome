461a3d5127a3af81dd36cb4640d1a534c2bd345d
Chedered <gwched@gmail.com>
2020-02-15 16:52:42 +0300

    Added main.py

--- main.py
+++ main.py
@@ -5,26 +5,30 @@ print ("Загадано число от 1 до 99")
 def game_core_v3(number):
     count = 0
     predict = np.random.randint(1,100)
-    x = abs(number - predict) #Вместо того, чтобы накручивать счётчик, прибавляем/убавляем разницу.
-    while number != predict:
+    dif = abs(number - predict) #Вместо того, чтобы накручивать счётчик, прибавляем/убавляем разницу.
+    #Так как счетчик не крутиться, если числа изначально равны, 
+    #а алгоритм всегда угадывает с первого раза,
+    #крутим счетчик при равенстве.
+    #Так количество попыток не будет падать ниже единицы на дистанции.
+    if number == predict:
         count+=1
-        if number > predict:
-            predict += x
-        elif number < predict:
-            predict -= x
+    else:
+        while number != predict:
+            count+=1
+            if number > predict:
+                predict += dif
+            elif number < predict:
+                predict -= dif
     return(count) # выход из цикла, если угадали
 
 def score_game(game_core):
     '''Запускаем игру 1000 раз, чтоб узнать как быстро игра угадывает число'''
     count_ls = []
     np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
-    random_array = np.random.randint(1, 101, size=(100))
+    random_array = np.random.randint(1, 101, size=(1000))
     for number in random_array:
         count_ls.append(game_core(number))
-    if int(np.mean(count_ls)) == 0:
-        score = 1
-    else:
-        score = int(np.mean(count_ls))
+    score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
     return(score)
 
