4560c7331ac5e479caf4dc59b20439e444f19342
Chedered <gwched@gmail.com>
2020-02-16 13:43:08 +0300

    Added main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import numpy as np
-number = np.random.randint(1,100) 
-print ("Загадано число от 1 до 99")
+number = np.random.randint(1,101) 
+print ("Загадано число от 1 до 100")
 
 #Бинарный поиск
 
 def game_core_v4(number):
     count = 0
     low = 1 # Нижняя граница поиска
-    high = 99 # Верхняя граница поиска
+    high = 100 # Верхняя граница поиска
     mid = 0 # Задаем переменную для середины, чтобы указать ее в условии цикла
     while number != mid and low < high: 
         count += 1
@@ -25,8 +25,8 @@ def score_game(game_core):
     random_array = np.random.randint(1, 101, size=(1000))
     for number in random_array:
         count_ls.append(game_core(number))
-    score = int(np.mean(count_ls))
+    score = (np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
     return(score)
 
-score_game(game_core_v4) #В среднем выходит 5.434
+score_game(game_core_v4) #В среднем выходит 5.441
